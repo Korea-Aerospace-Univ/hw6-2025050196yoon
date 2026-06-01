f9211db938b102771c24183d501b871b6d4fdbbb
2025050196yoon <choyoon@kau.kr>
2026-06-01 14:51:22 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+    char arr[10];
+    char *p, *q;
+    char maxChar;
+    int maxCount=0;
+    
+    for(p=arr ; p<arr+10 ; p++) {
+        scanf(" %c", p);
+    }
+    
+    for(p=arr ; p<arr+10 ; p++) {
+        int count=0;
+        
+        for(q=arr ; q<arr+10 ; q++) {
+            if (*p==*q) count++;
+        }
+        
+        if (count>maxCount) {
+            maxCount = count ; 
+            maxChar = *p ; 
+        }
+    }
+    printf("%c %d\n", maxChar, maxCount);
+    
+    return 0 ;
+}
