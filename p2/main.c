9d5e1ab22a8315b6ea86ac00af13b44dad48c9c8
2025050196yoon <choyoon@kau.kr>
2026-06-01 15:11:53 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) {
+    int n; 
+    int arr1[n]={}; 
+    int arr2[n]={};
+    int *p;
+    int *q;
+    
+    scanf("%d", &n);
+    
+    for(p=arr1 ; p<arr1+n ; p++) {
+        scanf("%d", &p);
+    }
+    
+    for(p=arr2 ; p<arr2+n ; p++) {
+        scanf("%d", &p);
+    }
+    
+    p = arr1
+    q = arr2 + N - 1 ; 
+    
+    while (p < arr1 + N)
+    {
+        printf(" %d", *p + *q);
+        p++;
+        q--;
+    }
+
+    return 0;
+    
+}
